8715903faff95004287070c46f6a500514eb13e0
priscilla254 <priscillawambui8@gmail.com>
2021-12-26 18:23:18 +0300

    final rgb guessing game

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const rElement=document.getElementById("r");
 const gElement=document.getElementById("g");
 const bElement=document.getElementById("b");
-
+const colorDispayElement=document.getElementById("color-display")
 const levels = Array.from(document.getElementsByClassName("mode"));
-const squares = Array.from(document.getElementsByClassName("square"));
+
 let selectedLevelButton=levels.find((level)=> {
     const classList=Array.from(level.classList);
     return classList.includes("selected");
-
 });
 
 let gameLevel=selectedLevelButton.innerHTML;
+
+let squares = getSquares()
 
 levels.forEach((level)=>{
     level.addEventListener("click",function(){
@@ -18,12 +19,43 @@ levels.forEach((level)=>{
         this.classList.add("selected");
 
         gameLevel=this.innerHTML;
+        setTilesAccordingToLevel(gameLevel)
+        squares=getSquares()
+
 
     });
-});
+});   
+
+function getSquares(){
+    const allSquares=Array.from(document.getElementsByClassName("square"));
+    if (gameLevel=="Easy"){
+        return allSquares.slice(0,3)
+    } else{
+        return allSquares
+    }
+
+}
+
+function setTilesAccordingToLevel(currentGameLevel){
+    //console.table([currentGameLevel])
+    const allSquares=Array.from(document.getElementsByClassName("square"));
+    if(currentGameLevel=='Easy'){
+        
+        const firstThreeSquares= allSquares.slice(0,3)
+        const lastThreeSquares=allSquares.slice(3,6)
+
+        lastThreeSquares.forEach(sq=> sq.classList.add("hidden"))
+    } else if (currentGameLevel="Hard"){
+        allSquares.forEach(sq=> sq.classList.remove("hidden"))
+        
+
+
+    }
+}
 
 const startButton=document.getElementById("reset");
 startButton.addEventListener("click", function(){
+    this.innerHTML="new colors";
     for (let i=0;i<squares.length;i++){
         const red= Math.floor(Math.random()*256);
         const green= Math.floor(Math.random()*256);
@@ -31,9 +63,66 @@ startButton.addEventListener("click", function(){
 
         const rgbString="rgb("+red+","+green+","+blue+")";
         
-        const square=squares[i];
-        square.style.backgroundColor=rgbString;
 
+        const square=squares[i];
+        // storing values in a HTML element
+        square.dataset.rgb_value=JSON.stringify([red,green,blue]);
+        square.style.backgroundColor=rgbString;
     }
-    
+    //assign the header a random rgb value from one of the square values
+    const randomSquareIndex=Math.floor(Math.random()*squares.length);
+    const headerColorSquare=squares[randomSquareIndex];
+    setHeaderRgbBackgroundColor(headerColorSquare);
 });
+
+function setHeaderRgbBackgroundColor(squareElement){
+    const setHeaderElementBackgroundColor=(rgbValues,element)=>{
+        const[r, g, b]=rgbValues;
+        const rgbString="rgb("+r+","+g+","+b+")";
+        //const rgbString='rgb( ${r},${g},${b})';
+        
+        element.style.backgroundColor=rgbString;
+        element.innerHTML=rgbValues.find((rgbValue)=>{
+            return rgbValue>0;
+        });
+    };
+    const rgbString=squareElement.dataset.rgb_value;
+    colorDispayElement.dataset.rgb_value=rgbString;
+    const [red,green,blue]=JSON.parse(rgbString);
+
+    const redBackground=[red,0,0];
+    const greenBackground=[0,green,0];
+    const blueBackground=[0,0,blue];
+
+    setHeaderElementBackgroundColor(redBackground,rElement);
+    setHeaderElementBackgroundColor(greenBackground,gElement);
+    setHeaderElementBackgroundColor(blueBackground,bElement);
+}
+
+// add eventlistener to squares so that it disappears or changes to uniform color
+
+squares.forEach(square =>{
+    square.addEventListener("click",function(){
+        const headerRgbValue=colorDispayElement.dataset.rgb_value
+        const squareRgbValue=this.dataset.rgb_value;
+
+        if (headerRgbValue==squareRgbValue){
+            setSquareBackgroundAfterWin(headerRgbValue);
+        }else{
+            this.classList.add("hidden")
+        }
+    });
+});
+
+function setSquareBackgroundAfterWin(headerRgbString){
+    const [r,g,b]=JSON.parse(headerRgbString);
+    const rgbString="rgb("+r+","+g+","+b+")";;
+    squares.forEach(sq=>{
+        sq.classList.remove("hidden")
+        sq.style.backgroundColor=rgbString;
+        sq.dataset.rgb_value=colorDispayElement.dataset.rgb_value
+         
+    });
+
+}
+       
